5d93d93c7f649e858d7a58b491784b9bfdb3b613
Michel Radosavljevic <mr@michel.se>
2013-07-16 13:39:25 +0200

    Created module DeveloperExtensions

---
+++ dotfiles/powershell/Modules/DeveloperExtensions/DeveloperExtensions.psm1
@@ -0,0 +1,22 @@
+﻿function Switch-Website {
+    <#
+    .SYNOPSIS
+    Switches to a specific website in IIS.
+    .DESCRIPTION
+    Closes running sites and starts the specified one.
+    .LINK
+    https://github.com/michelr/dotfiles
+    .LINK
+    Get-Website
+    Start-Website
+    Stop-Website
+    #>
+    param(
+        [ValidateSet("Friends","SthlmStad.Intranet")]
+        [Parameter(ValueFromPipeline=$true)]
+        [string]$Name
+    )
+    Get-Website | where State -eq Started | Stop-Website
+    Start-Website $Name
+    Get-Website $Name
+}
